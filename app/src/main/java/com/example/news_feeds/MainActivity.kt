bf01a6954e5381f6f1232d4a40108998ebc98e8c
Walid Zain <walidzeno@gmail.com>
2022-07-14 00:13:43 +0200

    ADD navigation drawer

---
+++ app/src/main/java/com/example/news_feeds/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.news_feeds
+
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import com.google.android.material.navigation.NavigationView
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.example.news_feeds.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.appBarMain.toolbar)
+
+
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+
+        appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.nav_home), drawerLayout)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+
+
+        binding.navView.setNavigationItemSelectedListener(this)
+    }
+
+
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment_content_main)
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_explore -> showToast(getString(R.string.menu_explore))
+            R.id.nav_live_chat -> showToast(getString(R.string.menu_live_chat))
+            R.id.nav_gallery -> showToast(getString(R.string.menu_gallery))
+            R.id.nav_wish_list -> showToast(getString(R.string.menu_wish_list))
+            R.id.nav_magazine -> showToast(getString(R.string.menu_magazine))
+
+        }
+        return true
+    }
+
+    private fun showToast(text: String) =
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+}
